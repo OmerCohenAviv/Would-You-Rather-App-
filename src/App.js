defb36bb7ee99cc3b2383703c786ec5c51666272
Pokernab <53443894+Pokernab@users.noreply.github.com>
2019-09-01 14:07:21 +0300

    Added Basic Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>NavigationBar         UserName+Avatar, logged ? logout : null</p>
+      <hr />
+      <p>Please Login in first -Logging in Form-</p>
+      <strong>Dont have a user yet? Create one -here button-</strong>
     </div>
   );
-}
+};
 
 export default App;
